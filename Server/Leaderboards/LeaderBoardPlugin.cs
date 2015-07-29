297ec67c2c45368c67992ff9e195c6386ec3605e
jmderuty <jmderuty@stormancer.com>
2015-07-29 20:30:20 +0200

    added leaderboard classes

---
+++ Server/Leaderboards/LeaderBoardPlugin.cs
@@ -0,0 +1,29 @@
+﻿using Stormancer;
+using Stormancer.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Leaderboards
+{
+    class LeaderBoardPlugin : IHostPlugin
+    {
+        public void Build(HostPluginBuildContext ctx)
+        {
+            ctx.SceneCreating += scene =>
+            {
+                scene.RegisterComponent<ILeaderboard>(() => new Leaderboard());
+            };
+        }
+    }
+
+    public class Startup
+    {
+        public void Run(IAppBuilder builder)
+        {
+            builder.AddPlugin(new LeaderBoardPlugin());
+        }
+    }
+}

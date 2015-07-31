71b1b27877c923cf06b556b47ecd76d382c90e2e
Antoine Lafarge <ant.lafarge@gmail.com>
2015-07-31 17:14:20 +0200

    fix leaderboard api ajax headers

--- Server/Assets/Admin/leaderboard/leaderboard.api.js
+++ Server/Assets/Admin/leaderboard/leaderboard.api.js
@@ -1,15 +1,17 @@
 var LeaderboardAPI = {
 	get: function(xToken, id, skip, take) {
-		var queryString = '?';
-		if (skip) queryString += "skip="+skip+'&';
-		if (take) queryString += "take="+take+'&';
-		//return $.get("https://api.stormancer.com/"+accountId+"/"+applicationName+"/_admin/leaderboard/"+(id?id:'')+queryString, {
-		return $.get("https://api.stormancer.com/"+accountId+"/"+applicationName+"/_admin/leaderboard/"+id+"/"+skip+"/"+take, {
+		var urlEnd = "";
+		if (id)
+		{
+			urlEnd += "/"+id+"/"+skip+"/"+take;
+		}
+		return $.ajax("https://api.stormancer.com/"+accountId+"/"+applicationName+"/_admin/leaderboard"+urlEnd, {
+			type: 'GET',
 			contentType: "application/json",
 			dataType: "json",
-			beforeSend: function(xhr) {
-				xhr.setRequestHeader("x-token", xToken);
-				xhr.setRequestHeader("x-version", "1.0");
+			headers: {
+				"x-token": "eyJFeHBpcmF0aW9uIjoiMjAxNS0wOC0wMVQxNDozNzoyMC40NDQzMDc3WiIsIklzc3VlZCI6IjIwMTUtMDctMzFUMTQ6Mzc6MjAuNDQ0MzA3N1oifQ%3D%3D.ZBcDMHmWZis8eR7icoG78qEh9qOXyzvoXagjUKOvdnc%3D",
+				"x-version": "1.0"
 			}
 		});
 	}

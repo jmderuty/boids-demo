c64c1b618ee085407d75946a40f15e2d442e8f25
Antoine Lafarge <ant.lafarge@gmail.com>
2015-07-31 19:44:31 +0200

    fix leaderboard api

--- Server/Assets/Admin/leaderboard/leaderboard.api.js
+++ Server/Assets/Admin/leaderboard/leaderboard.api.js
@@ -10,7 +10,7 @@ var LeaderboardAPI = {
 			contentType: "application/json",
 			dataType: "json",
 			headers: {
-				"x-token": "eyJFeHBpcmF0aW9uIjoiMjAxNS0wOC0wMVQxNDozNzoyMC40NDQzMDc3WiIsIklzc3VlZCI6IjIwMTUtMDctMzFUMTQ6Mzc6MjAuNDQ0MzA3N1oifQ%3D%3D.ZBcDMHmWZis8eR7icoG78qEh9qOXyzvoXagjUKOvdnc%3D",
+				"x-token": xToken,
 				"x-version": "1.0"
 			}
 		});

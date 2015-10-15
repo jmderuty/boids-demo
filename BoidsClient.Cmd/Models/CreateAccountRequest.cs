fd1fd6b247cbd9380bbd7f0f25e31a47bec70bd7
Jean-Noël Gourdol <jngourdol@stormancer.com>
2015-10-15 14:42:39 +0200

    refresh

---
+++ BoidsClient.Cmd/Models/CreateAccountRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stormancer.Authentication.Models
+{
+    public class CreateAccountRequest
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        //Json userdata
+        public string UserData { get; set; }
+    }
+}
